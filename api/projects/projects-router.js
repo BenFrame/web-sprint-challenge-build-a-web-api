b78ae66c563ee40be844e7ea6d767eff6201f477
Ben Frame <brewerytattooer@gmail.com>
2024-01-19 12:33:52 -0500

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -26,7 +26,57 @@ router.get('/:id', (req, res) => {
         .catch()
 })
 
+router.post('/', (req, res) => {
+    // console.log("post endpoint", req.body );
+    const{name, description, completed} = req.body
+    // eslint-disable-next-line no-prototype-builtins
+    if(!name || !description || ! req.body?.hasOwnProperty("completed") ){
+        res.status(400).json({
+            message: 'Please provide name, description, or completed'
+        })
+    }else{
+        Projects.insert({name, description, completed})
+        .then(({id}) =>{
+            return Projects.get(id)
+        })
+        .then(post => {
+            res.status(201).json(post)
+        })
+        .catch()
+    }
+})
 
+router.put('/:id', (req, res) =>{
+    const {name, description, completed} = req.body
+    // eslint-disable-next-line no-prototype-builtins
+    if(!name || !description || ! req.body?.hasOwnProperty("completed") ){
+        res.status(400).json({
+            message: 'Please provide name, description, or completed'
+        })
+    } else {
+        Projects.get(req.params.id)
+        .then(post => {
+            if(!post){
+                res.status(404).json({
+                    message: 'The project with the specified ID does not exists'
+                })
+            } else{
+                return Projects.update(req.params.id, req.body)
+            }
+        })
+        .then(data => {
+            if(data){
+                return Projects.get(req.params.id)
+            }
+        })
+        .then(post => {
+            if(post){
+                res.json(post)
+            }
+        })
+        .catch()
+    }
+})
 
 
 
