cd99f69e55026b83c9a73a22e421a35ef0a33c2c
Ben Frame <brewerytattooer@gmail.com>
2024-01-19 11:45:13 -0500

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,37 @@
-// Write your "projects" router here!
+const express = require('express')
+
+const Projects = require('../projects/projects-model')
+
+const router = express.Router()
+
+router.get('/', (req, res, ) => {
+    Projects.get()
+        .then(projects => {
+            res.json(projects)
+        })
+        .catch()
+})
+
+router.get('/:id', (req, res) => {
+    Projects.get(req.params.id)
+        .then(projects => {
+            if(!projects){
+                res.status(404).json({
+                    message: 'The project with the specified ID does not exists'
+                })
+            } else {
+                res.json(projects)
+            }
+        })
+        .catch()
+})
+
+
+
+
+
+
+
+
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const express = require('express')
-
-const server = express()
+const server = require('./api/server')
 
 const PORT = process.env.PORT || 9000
-
-server.use(express.json())
-
 
 server.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
